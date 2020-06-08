fcbc335170905cf7fa716f781d7e6fc1407f0210
Daniel Madera <dmadera0@gmail.com>
2020-06-08 20:49:46 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -14,8 +14,3 @@ def link
   puts phrase
 end  
   
-def all_phrases
-  phrase = "It's-a me, Mario"
-  phrase = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  phrase = "It's Dangerous To Go Alone! Take This."
-end
