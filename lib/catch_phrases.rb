0ae73dbe877c2a74ba485f946f6cc2d51eb63b84
Daniel Madera <dmadera0@gmail.com>
2020-06-08 20:45:28 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -17,6 +17,5 @@ end
 def all_phrases
   phrase = "It's Dangerous To Go Alone! Take This."
   phrase = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  phrase = "It's Dangerous To Go Alone! Take This."
-  puts phrase
+  phrase = "It's-a me, Mario!"
 end
